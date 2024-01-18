8c32278d47183597484b03aa3c0e169e76d70de9
A Mon <a.mont.24.777@gmail.com>
2024-01-18 16:54:05 -0600

    first commit

---
+++ Gear-Template-Frontend/src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+
+import { LocalBalanceToken } from "./MyBalanceToken";
+import { NameFungibleToken } from "./NameToken";
+
+function Home() {
+  return (
+    <LocalBalanceToken/>
+  );
+}
+
+export { Home };
